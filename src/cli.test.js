c8a30ce1824857e36bba35d00a04e237b0c21c52
Tyler Waters <tyler.waters@epicure.com>
2024-09-19 21:59:28 -0700

    Add support for reading from stdin, custom output path
    Fixes #102, #84

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process')
-const { ok, ifError, strictEqual: equal } = require('assert')
+const { ok, ifError, strictEqual: equal, throws } = require('assert')
 const path = require('path')
 const { existsSync, unlinkSync } = require('fs')
 const { getChecksumFilePath } = require('./checksum')
@@ -7,13 +7,15 @@ const package = require('../package.json')
 const { magenta, yellow } = require('chalk')
 const { encoding } = require('./defaults')
 
-const run = (cmd) => execSync(cmd, { encoding })
+const run = (cmd) => execSync(cmd, { encoding, stdio: 'pipe' })
 
 module.exports = () => {
   const scriptPath = path.join(__dirname, 'cli.js')
   const command = `node ${scriptPath}`
 
   const options = [
+    '--stdin',
+    '--output',
     '--file',
     '-f',
     '--color',
@@ -46,11 +48,74 @@ module.exports = () => {
 
     unlinkSync(checksumFilePath)
 
+    // verify output option works
+
+    const outputFile = path.resolve(__dirname, 'test-file.sha')
+    if (existsSync(outputFile)) unlinkSync(outputFile)
+
+    const thirdRunText = run(
+      `${command} --file ${scriptPath} --output ${outputFile} ${payload}`,
+    )
+    ok(thirdRunText.includes(payload))
+    ok(thirdRunText.includes(outputFile))
+
+    const fourthRunText = run(
+      `${command} -f ${scriptPath} --output ${outputFile} ${payload}`,
+    )
+    equal(fourthRunText, '')
+
+    unlinkSync(outputFile)
+
+    // verify stdin / output works
+
+    const fifthRunText = run(
+      `cat ${scriptPath} | ${command} --stdin --output ${checksumFilePath} ${payload}`,
+    )
+    ok(fifthRunText.includes(payload))
+    ok(fifthRunText.includes(checksumFilePath))
+
+    const sixthRunText = run(
+      `cat ${scriptPath} | ${command} --stdin --output ${checksumFilePath} ${payload}`,
+    )
+    equal(sixthRunText, '')
+
+    unlinkSync(checksumFilePath)
+
     const colorText = run(`${command} --color --file ${scriptPath} ${payload}`)
     ok(colorText.includes(magenta(scriptPath)))
     ok(colorText.includes(yellow(payload)))
 
     unlinkSync(checksumFilePath)
+
+    throws(
+      () => {
+        run(`${command} --stdin --file ${scriptPath} ${payload}`)
+      },
+      (err) => {
+        ok(err.stderr.includes('Either stdin or file must be provided'))
+        return true
+      },
+    )
+
+    throws(
+      () => {
+        run(`${command} ${payload}`)
+      },
+      (err) => {
+        ok(err.stderr.includes('Either stdin or file must be provided'))
+        return true
+      },
+    )
+
+    throws(
+      () => {
+        run(`${command} --stdin ${payload}`)
+      },
+      (err) => {
+        ok(err.stderr.includes('Output must be provided with stdin'))
+        return true
+      },
+    )
   } catch (error) {
     ifError(error)
   }
